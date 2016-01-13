877417eaffcf067d7e2f7a0b5502441d3057cf6a
Alex Samorukov <samm@os2.kiev.ua>
2016-01-13 01:48:07 +0100

    add support for other audiopedia web sites

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # staroeradio.ru downloader
 ## Description
-This script is created to download audio files from the http://staroeradio.ru/ web site and [related projects](http://www.audiopedia.su/). 
+This script is created to download audio files from the http://staroeradio.ru/ web site and [related projects](http://www.audiopedia.su/).
 ## Requirments
 Web site using RTMP streaming protocol, so rtmpdump is a requirment. To generate valid MP3 files with metadata FFMPEG is in use. Script itself is written on Perl and tested in OSX, but should be portable
 ## How to use
-TODO
+You should run it with -u <url>, e.g. 
+`./staroe-dl -u http://reportage.su/audio/1598`
 ## Author
 Author of the script is Alex Samorukov, samm@os2.kiev.ua. If you found any issue please submit it via [issue tracker](https://github.com/samm-git/staroeradio-dl/issues) or [pull request](https://github.com/samm-git/staroeradio-dl/pulls). 
--- staroe-dl.pl
+++ staroe-dl.pl
@@ -7,24 +7,48 @@ use Getopt::Std;
 
 # declare the perl command line flags/options we want to allow
 my %options=();
-getopts("hi:o:", \%options);
+getopts("hu:o:", \%options);
 
-if(defined $options{h} || not defined $options{i}){
-	print "Usage: $0 -i <mp3id> [-o filename]\n".
+if(defined $options{h} || not defined $options{u}){
+	print "Usage: $0 -u <url> [-o filename]\n".
 		"\tmp3id is http://www.staroeradio.ru/audio/<number>, by default output name taken from the file info\n\n";
 	exit 0;
 }
 
-my $mp3id = $options{i};
+my %urls = 
+(
+	'theatrologia.su' => 'theatrologia',
+	'staroeradio.ru' => '',
+	'svidetel.su' => 'svidetel',
+	'lektorium.su' => 'lektorium',
+	'reportage.su' => 'reportage'
+);
+my $site = ''; my $mp3id; my $domain;
+# check if we can parse URL
+if ($options{u} =~ m/(www\.)?(staroeradio\.ru|svidetel\.su|theatrologia\.su|lektorium\.su|reportage\.su)\/audio\/(\d+)/i) {
+  $mp3id = $3;
+  $site = $urls{$2};
+  $domain = $2;
+} else {
+  print "Wrong URL, exiting\n";
+  die();
+}
 
 # server using http://server.audiopedia.su:8888/getmp3parms.php?mp3id=<id> to get
-# parametr data
+# parametr data. There is also site parameter to set the namespace
+#
+# http://theatrologia.su/audio/<id> - getapmp3parms.php?site=theatrologia&mp3id=<id>
+# http://lektorium.su/audio/<id> - getapmp3parms.php?site=lektorium&mp3id=<id>
+# http://reportage.su/audio/<id> getapmp3parms.php?site=reportage&mp3id=<id>
+# http://svidetel.su/audio/<id> getapmp3parms.php?site=svidetel&mp3id=<id>
+# http://staroeradio.ru/audio/<id> - getapmp3parms.php?mp3id=<id>
+
 binmode STDOUT, ":utf8"; # suppress UTF-8 warnings
-my $content = get(" http://server.audiopedia.su:8888/getmp3parms.php?mp3id=".$mp3id);
+my $content = get("http://server.audiopedia.su:8888/".(length($site)?'getapmp3parms':'getmp3parms').".php?mp3id=".$mp3id.(length($site) ? "&site=$site" : ""));
 die "Unable to get file description, exiting\n" unless defined $content;
 my $ref = XMLin($content);
 print "Saving to '".$ref->{fname}."'\n";
 my $reqname = $ref->{fname};
 $reqname =~ s/\.mp3$//; # cut mp3 suffix
 system("rtmpdump -r 'rtmp://server.audiopedia.su/vod/' -y 'mp3:".$ref->{dir}."/".$reqname."' --live -o - | 
-    ffmpeg -loglevel warning -y -i - -acodec copy  -metadata title='$ref->{fullname}' -metadata artist='staroeradio.ru' '".$ref->{fname}."'");
+    ffmpeg -loglevel warning -y -i - -acodec copy  -metadata title='$ref->{fullname}' -metadata artist='$domain' '".$ref->{fname}."'");

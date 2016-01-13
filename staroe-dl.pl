d54917a4ee50afe75b344d7330ec30b7a8b8e5c4
Alex Samorukov <samm@os2.kiev.ua>
2016-01-13 17:44:39 +0100

    Add ability to specify output filename, espace path symbols in server-provided filename

--- staroe-dl.pl
+++ staroe-dl.pl
@@ -47,8 +47,15 @@ binmode STDOUT, ":utf8"; # suppress UTF-8 warnings
 my $content = get("http://server.audiopedia.su:8888/".(length($site)?'getapmp3parms':'getmp3parms').".php?mp3id=".$mp3id.(length($site) ? "&site=$site" : ""));
 die "Unable to get file description, exiting\n" unless defined $content;
 my $ref = XMLin($content);
-print "Saving to '".$ref->{fname}."'\n";
 my $reqname = $ref->{fname};
+my $savefname = '';
+if(defined $options{o}) {
+	$savefname = $options{o}
+} else {
+	$savefname = $ref->{fname};
+	$savefname =~ s/[\/\\']/_/g; # avoid "'" , / and \ characters in the output file name 
+}
+print "Saving to '".$savefname."'\n";
 $reqname =~ s/\.mp3$//; # cut mp3 suffix
 system("rtmpdump -r 'rtmp://server.audiopedia.su/vod/' -y 'mp3:".$ref->{dir}."/".$reqname."' --live -o - | 
-    ffmpeg -loglevel warning -y -i - -acodec copy  -metadata title='$ref->{fullname}' -metadata artist='$domain' '".$ref->{fname}."'");
+    ffmpeg -loglevel warning -y -i - -acodec copy  -metadata title='$ref->{fullname}' -metadata artist='$domain' '$savefname'");

a46cbd32bfed9b452c11621eda9223c59119054b
Alex Samorukov <samm@os2.kiev.ua>
2016-01-13 21:01:52 +0100

    run ffmpeg w/o shell

--- staroe-dl.pl
+++ staroe-dl.pl
@@ -44,8 +44,11 @@ if ($options{u} =~ m/(www\.)?(staroeradio\.ru|svidetel\.su|theatrologia\.su|lekt
 # http://staroeradio.ru/audio/<id> - getapmp3parms.php?mp3id=<id>
 
 binmode STDOUT, ":utf8"; # suppress UTF-8 warnings
-my $content = get("http://server.audiopedia.su:8888/".(length($site)?'getapmp3parms':'getmp3parms').".php?mp3id=".$mp3id.(length($site) ? "&site=$site" : ""));
+my $content = get("http://server.audiopedia.su:8888/".
+	(length($site)?'getapmp3parms':'getmp3parms').
+	".php?mp3id=".$mp3id.(length($site) ? "&site=$site" : ""));
 die "Unable to get file description, exiting\n" unless defined $content;
+
 my $ref = XMLin($content);
 my $reqname = $ref->{fname};
 my $savefname = '';
@@ -55,6 +58,11 @@ if(defined $options{o}) {
 	$savefname = $ref->{fname};
 	$savefname =~ s/[\/\\']/_/g; # avoid "'" , / and \ characters in the output file name 
 }
+
 print "Saving to '".$savefname."'\n";
 $reqname =~ s/\.mp3$//; # cut mp3 suffix
-system("ffmpeg -rtmp_playpath 'mp3:".$ref->{dir}."/".$reqname."' -i 'rtmp://server.audiopedia.su/vod/'  -y  -acodec copy  -metadata title='$ref->{fullname}' -metadata artist='$domain' '$savefname'");
+# finally run ffmpeg, without shell
+system("ffmpeg", "-rtmp_playpath", "mp3:".$ref->{dir}."/$reqname",
+	"-i", "rtmp://server.audiopedia.su/vod/", "-y", "-acodec", "copy",
+	"-metadata", "title='$ref->{fullname}'", "-metadata", "artist='$domain'", 
+	$savefname);
